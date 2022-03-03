6f820627d8fabd8bdc9649cad5d3ce9091d11ce0
Ozzymnds <sanz00zgz@gmail.com>
2022-03-03 18:57:57 +0100

    Commit final

--- src/Examen2EVA/Ejercicio2.java
+++ src/Examen2EVA/Ejercicio2.java
@@ -5,23 +5,32 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Ejercicio2 {
-	public static String fichero = "C:\\Users\\jmsan\\Desktop\\ficherosExamen\\EjerFicheroTexto"
+	public static String fichero = "C:\\Users\\jmsan\\Desktop\\ficherosExamen\\EjerFicheroTexto" 
 			+ "\\textoExamen.txt";
-	
+
 	static Scanner sc = new Scanner(System.in);
-	
+
+	/*
+	 * Método usado para leer el fichero línea a línea, luego lo separamos con
+	 * '.split' usando como referencia las comas (","), para finalizar le pedimos
+	 * que nos devuelva el 'contador', el cual se encarga de enumerar las palabras
+	 * que tengan una longitud superior a la pedida por scanner.
+	 * 
+	 */
 	public static int palabrasMasLargasQue(int longitud) {
-		//int length = Integer.parseInt(longitud);
-		int contador = 0;
-		File doc = new File(fichero);	
-		//int largura = Integer.parseInt(largo);
+		// int length = Integer.parseInt(longitud);
+		int contador = 0; // Encargado de contar las palabras en base a la longitudad dada.
+		File doc = new File(fichero);
+		// int largura = Integer.parseInt(largo);
 		try {
 			Scanner d = new Scanner(doc);
-			while(d.hasNextLine()) {
+			// Si el fichero de texto tiene una línea más a continuación, se abre bucle.
+			while (d.hasNextLine()) {
 				String linea = d.nextLine();
+				// Usamos el '.split' para separar las palabras del texto por espacios.
 				String[] lineaSep = linea.split(" ");
-				//int lineSep = Integer.parseInt(linea);
-				for(int i = 0; i < lineaSep.length; i++) {
+				// int lineSep = Integer.parseInt(linea);
+				for (int i = 0; i < lineaSep.length; i++) {
 					System.out.println(lineaSep[i]);
 					if (longitud > (lineaSep[i].length())) {
 						contador++;
@@ -36,6 +45,13 @@ public class Ejercicio2 {
 		}
 		return contador;
 	}
+
+	/*
+	 * Pre: --- Post: Nos piden un programa Java que lea un fichero de texto para
+	 * llevarlo a un método al cual llamaremos 'palabrasMasLargasQue()', para que
+	 * nos muestre el número de palabras que son más largas que la longitud que
+	 * habrá de meter el usuario por scanner.
+	 */
 	public static void main(String args[]) {
 		System.out.println("Contador de palabras más largas que 5");
 		System.out.println("Dime la longitud");
